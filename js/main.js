bd2cf518740b5923a210bb81f9733882cdb3d2a1
Miggie Garcia <60056056+miggiegarcia@users.noreply.github.com>
2020-03-08 23:31:06 -0400

    updating comments on main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const key = "EOQLqyu59VbvxKO18vFcbvs7n8nGC8ww";
 button.addEventListener('click',function(){
       // console.log(pelicula.value);
 document.createElement("LI")
-let ul =document.getElementById("resultList")
+let ul =document.getElementById("resultList") // cors is important for authentication per the documentation of the API.. Abbey helped me to understand the importance of the cors for authentication
 fetch(`https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${pelicula.value}&api-key=${key}`)
     .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
     .then(response => {
@@ -24,13 +24,13 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/movies/v2
         let newContent = document.createTextNode(response.results[i].link.url);
             a.appendChild(newContent);
           a.href= url
-
+// alumni Raymond helped me with this section in order to have the user click on the link for the article beside it display the url but the user can actually visit the article on the new york times website
         li.innerHTML=response.results[i].display_title
 
 
 
 
-        // // add the text node to the newly created div
+        // add the text node to the newly created div
 
         li.appendChild(a);
         ul.appendChild(li);
@@ -55,4 +55,5 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/movies/v2
 //
 //   li.appendChild(newLi);
 //   ul.appendChild(li);
-// }
+// } this code portion is from my to do list project which i used to help clean up the code here for the user side of the application. 
+
